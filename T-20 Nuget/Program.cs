c517401aabdf1e90b330a34db0937f39d7d5d5a2
Jepolancodlc <Jepolancodlc@gmail.com>
2021-02-07 17:26:34 +0100

    0

---
+++ T-20 Nuget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace T_20_Nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
